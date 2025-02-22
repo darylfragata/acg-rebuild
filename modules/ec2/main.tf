fe9bde418428404c9084ba43d4c9165ade9c0ccd
Daryl <151862534+darylfragata@users.noreply.github.com>
2025-02-23 01:41:02 +0800

    Update main.tf
    Update key_name to module_key_pair

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,11 +3,11 @@ resource "aws_instance" "compute" {
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = var.security_group_ids
-  key_name                    = var.key_name
+  key_name                    = var.module_key_pair
   iam_instance_profile        = var.iam_instance_profile
   #associate_public_ip_address = var.associate_public_ip
 
   tags = var.tags
 
-  depends_on = [ var.key_name ]
+  depends_on = [ var.module_key_pair ]
 }
