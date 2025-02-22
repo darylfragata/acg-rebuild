0b84e6d810e1973c1b8192f765947f17bb19b6ed
Daryl <151862534+darylfragata@users.noreply.github.com>
2025-02-23 01:08:11 +0800

    Update backend.tf

--- environments/dev/security_groups/backend.tf
+++ environments/dev/security_groups/backend.tf
@@ -1,5 +1,5 @@
 # terraform {
-#   backend "remote" {
+#   backend "s3" {
 #     bucket         = "dev-tfstatefile-021925"
 #     key            = "dev/sg.tfstate"
 #     region         = "us-east-1"
